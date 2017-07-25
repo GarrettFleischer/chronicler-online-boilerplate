ec3b4655fed8ad85153233de5e81ab5565dd74b5
GarrettFleischer <garrett.fleischer@oit.edu>
2017-07-24 17:09:30 -0700

    Added Component component.

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,9 +1,10 @@
-/* eslint-disable */
 const exec = require('child_process').exec;
+// noinspection JSUnusedLocalSymbols
 exec('npm -v', (err, stdout, stderr) => {
   if (err) throw err;
   if (parseFloat(stdout) < 3) {
     throw new Error('[ERROR: React Boilerplate] You need npm version @>=3');
+    // noinspection UnreachableCodeJS
     process.exit(1);
   }
 });
